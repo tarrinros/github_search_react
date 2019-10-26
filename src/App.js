ce09b8f2f4acd06e65c191b5f987c55a1841c852
tarrinros <tarindis@gmail.com>
2019-10-26 16:14:12 +0300

    Add alert message component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import {Navbar} from './components/Navbar'
 import {Home} from './pages/Home'
 import {About} from './pages/About'
 import {Profile} from './pages/Profile'
+import { Alert } from './components/Alert'
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <div className="container pt-4">
+        <Alert alert={{text: 'Test alert'}} />
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/about" component={About} />
