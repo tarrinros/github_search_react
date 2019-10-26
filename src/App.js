245c44050053819d3e564500ec1ea50004ceee43
tarrinros <tarindis@gmail.com>
2019-10-26 16:21:41 +0300

    Add Alert context and state

--- src/App.js
+++ src/App.js
@@ -5,20 +5,23 @@ import {Home} from './pages/Home'
 import {About} from './pages/About'
 import {Profile} from './pages/Profile'
 import { Alert } from './components/Alert'
+import { AlertState } from './context/alert/alertState'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <div className="container pt-4">
-        <Alert alert={{text: 'Test alert'}} />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/profile/:name" component={Profile} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <AlertState>
+      <BrowserRouter>
+        <Navbar />
+        <div className="container pt-4">
+          <Alert alert={{text: 'Test alert'}} />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/profile/:name" component={Profile} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </AlertState>
   );
 }
 
