497cf6a2c30a287be1cfef43e692271161ac28e2
tarrinros <tarindis@gmail.com>
2019-10-26 18:13:16 +0300

    Add rendering users cards by request results

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,25 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useContext} from 'react'
 import { Search } from '../components/Search'
 import { Card } from '../components/Card'
+import { GithubContext } from '../context/github/githubContext'
 
 export const Home = () => {
-  const cards = new Array(15)
-    .fill('')
-    .map((_, i) => i)
-  console.log(cards)
+  const {loading, users} = useContext(GithubContext)
+
   return (
     <Fragment>
       <Search />
 
       <div className="row">
-        {cards.map((card) => {
-          return (
-            <div className="col-sm-4 mb-4" key={card}>
-              <Card />
-            </div>
-          )
-        })}
 
-        <div className="col-sm-4 mb-4">
-          <Card />
-        </div>
-
-        <div className="col-sm-4 mb-4">
-          <Card />
-        </div>
+        {loading
+          ? <p className="text-center">Loading...</p>
+          : users.map(user => (
+              <div className="col-sm-4 mb-4" key={user.id}>
+                <Card user={user}/>
+              </div>
+            ))
+        }
       </div>
     </Fragment>
   )
