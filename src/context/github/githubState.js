37bf3bf12f852164bcb6c79bdb10d9becbe3c9c5
tarrinros <tarindis@gmail.com>
2019-10-26 17:32:24 +0300

    Add methods for dispatching states

--- src/context/github/githubState.js
+++ src/context/github/githubState.js
@@ -1,6 +1,8 @@
 import React, {useReducer} from 'react'
 import { GithubContext } from './githubContext'
 import { githubReducer } from './githubReducer'
+import { SEARCH_USERS, GET_USER, GET_REPOS, CLEAR_USERS, SET_LOADING } from '../types'
+import { async } from 'q'
 
 export const GithubState = ({children}) => {
   const initialState = {
@@ -11,9 +13,38 @@ export const GithubState = ({children}) => {
   }
   const [state, dispatch] = useReducer(githubReducer, initialState)
 
+  const search = async value => {
+    dispatch({
+      type: SEARCH_USERS,
+      payload: []
+    })
+  }
+
+  const getUser = async name => {
+    dispatch({
+      type: GET_USER,
+      payload: {}
+    })
+  }
+
+  const getRepos = async name => {
+    dispatch({
+      type: GET_REPOS,
+      payload: []
+    })
+  }
+  
+  const clearUsers = () => dispatch({type: CLEAR_USERS})
+  
+  const setLoading = () => dispatch({type: SET_LOADING})
+
+  // Destructuring of state
+  const {user, users, repos, loading} = state
+
   return (
     <GithubContext.Provider value={{
-
+      search, setLoading, getUser, getRepos, clearUsers, setLoading,
+      user, users, repos, loading
     }}>
       {children}
     </GithubContext.Provider>
