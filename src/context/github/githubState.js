eb55d42aada7b347b75699e805904696c09a6c85
tarrinros <tarindis@gmail.com>
2019-10-26 18:03:28 +0300

    Add requests for searching users by axios

--- src/context/github/githubState.js
+++ src/context/github/githubState.js
@@ -7,6 +7,10 @@ import { SEARCH_USERS, GET_USER, GET_REPOS, CLEAR_USERS, SET_LOADING } from '../
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET
 
+const withCreads = url => {
+  return `${url}client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+}
+
 export const GithubState = ({children}) => {
   const initialState = {
     user: {},
@@ -20,7 +24,7 @@ export const GithubState = ({children}) => {
     setLoading()
 
     const response = await axios.get(
-      `https://api.github.com/search/users?q=${value}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+      withCreads(`https://api.github.com/search/users?q=${value}&`)
     )
 
     dispatch({
@@ -32,18 +36,26 @@ export const GithubState = ({children}) => {
   const getUser = async name => {
     setLoading()
 
+    const response = await axios.get(
+      withCreads(`https://api.github.com/users/${name}?`)
+    )
+
     dispatch({
       type: GET_USER,
-      payload: {}
+      payload: response.data
     })
   }
 
   const getRepos = async name => {
     setLoading()
 
+    const response = await axios.get(
+      withCreads(`https://api.github.com/users/${name}/repos?per_page=5&`)
+    )
+
     dispatch({
       type: GET_REPOS,
-      payload: []
+      payload: response.data
     })
   }
   
