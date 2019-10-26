cee0957ff56df9df95af20676fc75abcb4bff49d
tarrinros <tarindis@gmail.com>
2019-10-26 16:54:49 +0300

    Add alert to the project using react hooks

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { AlertContext } from '../context/alert/alertContext'
 
-export const Alert = ({alert}) => {
+export const Alert = () => {
+  const {alert, hide} = useContext(AlertContext)
+
+  if (!alert) return null
+
   return (
     <div 
       className={`alert alert-${alert.type || 'secondary'} alert-dismissible`} 
       role="alert"
     >
       {alert.text}
-      <button type="button" className="close" aria-label="Close">
+      <button type="button" className="close" aria-label="Close" onClick={hide}>
         <span aria-hidden="true">&times;</span>
       </button>
     </div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { AlertContext } from '../context/alert/alertContext'
 
 export const Search = () => {
   
+  const {show} = useContext(AlertContext)
+
+  const onSubmit = event => {
+    if (event.key === 'Enter') {
+      show('This is alert!')
+    }
+  }
+
   return (
     <div className="form-group">
       <input 
         type="text"
         className="form-control"
         placeholder="Enter users nickname"
+        onKeyPress={onSubmit}
       />
     </div>
   )
