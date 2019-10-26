ea1b3db9e0583675c62954a86e74741012a4c1c7
tarrinros <tarindis@gmail.com>
2019-10-26 15:39:35 +0300

    Add and configure react router

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
 
 export const Navbar = () => (
   <nav className="navbar navbar-dark bg-primary navbar-expand-lg">
@@ -8,10 +9,10 @@ export const Navbar = () => (
 
     <ul className="navbar-nav">
       <li className="nav-item">
-        <a href="/" className="navbar-link">Main</a>
+        <NavLink exact to="/" className="nav-link">Main</NavLink>
       </li>
       <li className="nav-item">
-        <a href="/about" className="navbar-link">Info</a>
+        <NavLink to="/about" className="nav-link">Info</NavLink>
       </li>
     </ul>
   </nav>
--- src/pages/About.js
+++ src/pages/About.js
@@ -3,8 +3,11 @@ import React from 'react'
 export const About = () => {
   
   return (
-    <div>
-      <h1>About page</h1>
-    </div> 
+<div className="jumbotron jumbotron-fluid">
+  <div className="container">
+    <h1 className="display-4">Info</h1>
+    <p className="lead">Application version: <strong>1.0.0</strong></p>
+  </div>
+</div>
   )
 }
